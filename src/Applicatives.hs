0bdb77341cb0ffd728f0bb753391f3ca1261abb0
Erik Svedäng <erik.svedang@gmail.com>
2025-10-03 21:39:34 +0200

    More examples.

--- src/Applicatives.hs
+++ src/Applicatives.hs
@@ -4,3 +4,7 @@ module Applicatives where
 -- (<*>) :: Applicative f => f (a -> b) -> f a -> f b
 
 noMonads = pure (<>) <*> getLine <*> getLine
+noMonadsAgain = pure reverse <*> getLine
+
+alsoNoMonads = pure (*) <*> [1, 2, 3] <*> [10, 1000]
+sameButDifferent = (*) <$> [1::Int, 2, 3] <*> [10, 1000]
